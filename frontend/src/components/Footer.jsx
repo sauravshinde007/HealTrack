f402e974e82585ee8d9ced3bd4eba24ec9505787
Saurav Shinde <sauravshinde.000@gmail.com>
2025-04-20 23:08:29 +0530

    Added Project Files

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>HealTrack is a dedicated Clinic Management System designed to streamline and simplify operations for individual clinics. From managing patient records and appointments to handling billing and staff schedules, HealTrack offers an all-in-one solution tailored for small to mid-sized healthcare practices. With a clean interface and smart features, it helps clinic staff focus more on patient care and less on paperwork.
+
+</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>+1-212-456-7890</li>
+            <li>sarangshukla45@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2024 @ HealTrack.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
